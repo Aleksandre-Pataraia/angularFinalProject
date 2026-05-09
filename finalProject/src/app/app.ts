90664d3ec59c2e63d9a0a27cfa85f5983a35d440
spataraia16 <a.pataraia16@gmail.com>
2026-05-09 16:13:30 +0400

    Draft4

--- finalProject/src/app/app.ts
+++ finalProject/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "./body/header/header";
+import { Footer } from "./body/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
